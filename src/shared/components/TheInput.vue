9d5cc4ff9dd93358137f73781c0c638da75ff848
Roman <55388694+Roman-wdesign@users.noreply.github.com>
2023-10-06 14:02:47 +0700

    Input was added 1

---
+++ src/shared/components/TheInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+export interface Props {
+    modelValue: string
+    label: string
+}
+const props = defineProps<Props>()
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event: Event) => {
+    emit('update:modelValue', (event.target as HTMLInputElement).value)
+}
+</script>
+
+<template>
+    <div class="mr-4">
+        <slot name="header"></slot>
+    </div>
+    <input class="input border-2 border-amber-800" :placeholder="props.label" type="input" :value="modelValue"
+        @input="updateValue" />
+</template>
---
+++ src/shared/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TheInput from '@/shared/components/TheInput.vue';
+const thirdMessageInput = ref('')
+</script>
+
+<template>
+  <div class="container mx-auto px-4">
+    <div class="flex justify-center">
+      <h5>The Welcome</h5>
+      <TheInput label="введите число" v-model="thirdMessageInput"><template #header>Сметана 20%</template>
+      </TheInput>
+    </div>
+  </div>
+</template>
