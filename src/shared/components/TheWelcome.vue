cb4530d9402c173046a0a8fd6dae3a9c95ef97e1
Roman <55388694+Roman-wdesign@users.noreply.github.com>
2023-10-26 20:19:03 +0700

    Date builder

--- src/shared/components/TheWelcome.vue
+++ src/shared/components/TheWelcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import TheInput from '@/shared/components/TheInput.vue';
 import { apiKeys } from '@/helpers/config';
 
@@ -17,48 +17,58 @@ const setResults = (results: any) => {
 
 const fetchWeather = (e: any) => {
   if (e.key == 'Enter') {
-    fetch(`${urlBase}weather?q=${theQuery.value}&units=metric&APPID=${token}`)
+    fetch(`${urlBase}weather?q=${theQuery.value}&units=metric&APPID=${token}&lang={ru}`)
       .then(response => {
         return response.json()
       }).then(setResults)
   }
 }
 
+const dateBuilder = () => {
+  let d = new Date();
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-// onMounted(async () => {
-//   await fetchWeather('')
-//   console.log('Weather List was mounted')
-// })
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
+
+onMounted(async () => {
+  await fetchWeather('')
+  console.log('Weather List was mounted')
+})
 
 </script>
 
 <template>
   <div class="container mx-auto px-4">
-    <!-- <input class="input border-2 text-black border-amber-800" type="text" @keypress="fetchWeather" label="Город"
-      v-model="theQuery" /> -->
     <TheInput @keypress="fetchWeather" label="название города" v-model="theQuery">
     </TheInput>
 
 
-    <div class="flex justify-between py-6">
+    <div class="flex-col justify-between py-6">
+      <div class="date">
+        <h5 class="font-bold text-center italic text-sm">{{ dateBuilder() }}</h5>
+      </div>
       <div class="weather-wrap" v-if="(typeof theWeather.main != 'undefined')">
         <div class="location-box">
           <div class="location ">
             <h3 class="text-lg font-bold text-center">{{ theWeather.name }}, {{ theWeather.sys.country }}</h3>
-
-          </div>
-          <div class="date">
-            <h5 class="font-bold text-center italic text-sm">Wednesday 11 Oktober 2023</h5>
           </div>
         </div>
 
       </div>
-      <div class="weather-box  py-3 flex-col justify-center" v-if="(typeof theWeather.main != 'undefined')">
+      <div class="weather-box py-3 flex-col justify-center" v-if="(typeof theWeather.main != 'undefined')">
         <div class="temp flex justify-center py-3">
-          <h2 class="text-2xl font-extrabold">{{ theWeather.main.temp }}°</h2>
+          <h2 class="text-2xl font-extrabold">{{ Math.round(theWeather.main.temp) }} °c</h2>
         </div>
         <div class="weather">
-          <h4 class="flex justify-center font-bold">{{ theWeather.weather.main }}</h4>
+          <h4 class="flex justify-center font-bold">{{ theWeather.weather[0].main }}</h4>
         </div>
       </div>
     </div>
