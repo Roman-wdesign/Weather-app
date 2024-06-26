e2c7de702e1b178962ccae854f2d70b46c9a758c
Roman <gabbasov.tomsk@gmail.com>
2024-06-26 11:06:15 +0700

    geolocation init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TodayView from '../shared/views/TodayView.vue'
+import NowWeather from '../shared/views/NowWeatherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: TodayView
+      component: NowWeather
     },
    
   ]
