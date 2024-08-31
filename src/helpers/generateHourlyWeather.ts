9229e213d837211665717f2cd87d5dd985840375
Roman <gabbasov.tomsk@gmail.com>
2024-08-31 13:02:46 +0700

    tests generateWeatherUrl

--- src/helpers/generateHourlyWeather.ts
+++ src/helpers/generateHourlyWeather.ts
@@ -1,3 +1,3 @@
-export const generateHourlyWeather = (urlBase:string, lat: number, lon: number,  token: string): string => {
+export const generateHourlyWeather = (urlBase: string, lat: number, lon: number, token: string): string => {
   return `${urlBase}forecast?lat=${lat}&lon=${lon}&appid=${token}`
 }
--- src/helpers/vars.ts
+++ src/helpers/vars.ts
@@ -1,12 +1,12 @@
-export  const token: string = import.meta.env.VITE_WEATHER_SECRET_API_KEY
+export const token: string = import.meta.env.VITE_WEATHER_SECRET_API_KEY
 
 
 // url base 
-export  const urlBase: string = 'https://api.openweathermap.org/data/2.5/'
+export const urlBase: string = 'https://api.openweathermap.org/data/2.5/'
 
 
 // url to img
-export  const imgUrl: string = 'https://openweathermap.org/img/wn/'
+export const imgUrl: string = 'https://openweathermap.org/img/wn/'
 
 // url to geo
 export const reverseGeo: string = 'http://api.openweathermap.org/geo/1.0/'
