846681f5f2303ac7f0ab1911a12e36a975260885
Roman <gabbasov.tomsk@gmail.com>
2024-08-31 15:48:30 +0700

    autodelete all console.log before prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   plugins: [
     vue(),
   ],
