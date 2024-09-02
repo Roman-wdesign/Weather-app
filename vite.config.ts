b3f56d37dc4da85a589686c1a82d49efab35de0e
Roman <gabbasov.tomsk@gmail.com>
2024-09-02 10:27:03 +0700

    description of autodelete

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
+
   plugins: [
     vue(),
   ],
