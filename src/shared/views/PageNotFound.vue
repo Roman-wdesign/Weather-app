affc3d531f87aaeeea00a08b1e88d89a843b3fe7
ChaudaryHammad <ch.hammadbrw01@gmail.com>
2024-09-02 15:45:17 +0500

    dark mode fixed

--- src/shared/views/PageNotFound.vue
+++ src/shared/views/PageNotFound.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="grid h-[80vh] place-content-center bg-white px-4">
-    <div class="text-center">
-      <h1 class="text-9xl font-black text-gray-200">404</h1>
-  
-      <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</p>
-  
-      <p class="mt-4 text-gray-500">Try searching again, or return home to start from the beginning.</p>
-  
-      <router-link to="/"
-        class="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
-      >
-        Go Back Home
-      </router-link>
+  <div class="min-h-[80vh] bg-white dark:bg-gray-800 flex flex-col">
+    <div class="grid h-[80vh] place-content-center px-4">
+      <div class="text-center">
+        <h1 class="text-9xl font-black text-gray-200 dark:text-gray-400">404</h1>
+        <p class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
+          Uh-oh!
+        </p>
+        <p class="mt-4 text-gray-500 dark:text-gray-300">
+          Try searching again, or return home to start from the beginning.
+        </p>
+        <router-link
+          to="/"
+          class="mt-6 inline-block rounded bg-indigo-600 dark:bg-indigo-500 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 dark:hover:bg-indigo-400 focus:outline-none focus:ring"
+        >
+          Go Back Home
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
